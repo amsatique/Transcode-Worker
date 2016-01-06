ca2c18aabbc982c5dd60c4e06755e01e75e0a2fb
amsatique <amsatique@amsatique-2.local>
2016-01-06 16:17:54 +0100

    Bug fix

--- transcode.sh
+++ transcode.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 INPUT="$INPUT"
-#INPUT=test.avi # A retirer
 EXTENSION="$EXTENSION"
 NAME=$(echo $INPUT | cut -d '.' -f1)
-FFMPEG=ffmpeq -i /incomplete/$INPUT
-if [-n "{$INPUT}"];
+FFMPEG="ffmpeg -i /incomplete/$INPUT"
+if [ -n "{$INPUT}" ];
 then
-  if [-n "${NOVIDEO}"];
+  if [ -n "${NOVIDEO}" ];
   then
     NOVIDEO='-vn'
     FFMPEG=$FFMPEG $NOVIDEO
   fi
-  if [-n "${NOSOUND}"];
+  if [ -n "${NOSOUND}" ];
   then
     NOSOUND='-an'
     FFMPEG=$FFMPEG $NOSOUND
   fi
-  if [-n "${BITRATE}"];
+  if [ -n "${BITRATE}" ];
   then
     BITRATE='-b:v ${BITRATE}k'
     FFMPEG=$FFMPEG $BITRATE
   fi
-  if [-z "${FRAMERATE}"];
+  if [ -n "${FRAMERATE}" ];
   then
     FRAMERATE='-r ${FRAMERATE}'
     FFMPEG=$FFMPEG $FRAMERATE
   fi
-  FFMPEG=$FFMPEG /complete/$NAME.$EXTENSION
+  FFMPEG="$FFMPEG /complete/$NAME.$EXTENSION"
   $FFMPEG
-else
-  bin/bash
 fi
