8d1bb017d4b35f7e816ffc621a8d63a1923ec224
amsatique <amsatique@amsatique-2.local>
2016-01-06 14:33:15 +0100

    aa

--- transcode.sh
+++ transcode.sh
@@ -3,20 +3,20 @@ INPUT="$INPUT"
 #INPUT=test.avi # A retirer
 EXTENSION="$EXTENSION"
 NAME=$(echo $INPUT | cut -d '.' -f1)
-FFMPEG=ffmpeq -y -i /incomplete/$INPUT
-if [-z "$INPUT"];
+FFMPEG=ffmpeq -i /incomplete/$INPUT
+if [-n "{$INPUT}"];
 then
-  if [-z "${NOVIDEO}"];
+  if [-n "${NOVIDEO}"];
   then
     NOVIDEO='-vn'
     FFMPEG=$FFMPEG $NOVIDEO
   fi
-  if [-z "${NOSOUND}"];
+  if [-n "${NOSOUND}"];
   then
     NOSOUND='-an'
     FFMPEG=$FFMPEG $NOSOUND
   fi
-  if [-z "${BITRATE}"];
+  if [-n "${BITRATE}"];
   then
     BITRATE='-b:v ${BITRATE}k'
     FFMPEG=$FFMPEG $BITRATE
