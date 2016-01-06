16be82daa1f254c0b0ca6efb2ae37cce91a437f5
amsatique <amsatique@amsatique-2.local>
2016-01-06 16:43:35 +0100

    Upgrade script

--- transcode.sh
+++ transcode.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 INPUT="$INPUT"
+CUSTOMER="$CUSTOMER"
 EXTENSION="$EXTENSION"
 NAME=$(echo $INPUT | cut -d '.' -f1)
-FFMPEG="ffmpeg -i /incomplete/$INPUT"
+FFMPEG="ffmpeg -i /progress/$CUSTOMER/$INPUT"
+
+mv /incomplete/$INPUT /progress/$CUSTOMER/$INPUT
+if [ ! -d /complete/$CUSTOMER ];
+then
+  mkdir -p /complete/$CUSTOMER
+fi
+
 if [ -n "{$INPUT}" ];
 then
   if [ -n "${NOVIDEO}" ];
@@ -25,6 +33,8 @@ then
     FRAMERATE='-r ${FRAMERATE}'
     FFMPEG=$FFMPEG $FRAMERATE
   fi
-  FFMPEG="$FFMPEG /complete/$NAME.$EXTENSION"
+  FFMPEG="$FFMPEG /complete/$CUSTOMER/$NAME.$EXTENSION"
   $FFMPEG
+  if [ -f /complete/$CUSTOMER/$NAME.$EXTENSION ]
+  rm -f /imcomplete/$INPUT
 fi
